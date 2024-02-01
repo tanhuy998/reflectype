65018ba9c4685115b83a7cb79421d70360a95c49
Huy Tran <tanhuy998@gmail.com>
2024-02-02 03:45:48 +0700

    configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "no-console": "warn",
+        "no-unused-vars": "warn",
+        "no-undef": "error",
+        "no-constant-condition": "off",
+        "no-this-before-super": "error",
+        "no-unused-private-class-member": "warning",
+        "block-scoped-var": "error",
+        "no-var": "error",
+        "prefer-const": "error",
+        "vars-on-top": "error",
+        "constructor-super": "error",
+        "getter-return": "error",
+        "no-debugger": "warn",
+        "no-dupe-class-members": "error",
+        "no-dupe-keys": "error",
+        "no-dupe-case": "error",
+        "no-duplicate-imports": "error",
+        "no-irregular-whitespace": "warn",
+        "no-new-symbol": "error",
+    }
+}
